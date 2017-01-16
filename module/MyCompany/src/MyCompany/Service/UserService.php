a157a81008115803449e8e44d8aee30d8dcef119
Ruslan Kiricenko <ruslankus@yahoo.com>
2017-01-17 00:31:16 +0200

    update

--- module/MyCompany/src/MyCompany/Service/UserService.php
+++ module/MyCompany/src/MyCompany/Service/UserService.php
@@ -9,10 +9,16 @@
 namespace MyCompany\Service;
 
 
+use AssetManager\Exception\RuntimeException;
 use Doctrine\ORM\EntityManagerInterface;
+use MyCompany\Entity\User;
 
 class UserService implements UserServiceInterface
 {
+
+    const ERROR_USER_EXIST_CODE = 2;
+
+    const ERROR_USER_EXIST_MSG = "User with such email is allready exist";
 
     protected $entityManager;
 
@@ -23,7 +29,25 @@ class UserService implements UserServiceInterface
 
     public function registerUser($emailAddress, $password)
     {
-        // TODO: Implement registerUser() method.
+        $userObj = $this->entityManager->getRepository(User::class)
+            ->findOneBy(['email' => $emailAddress]);
+
+        if($userObj instanceof User){
+            throw new RuntimeException(self::ERROR_USER_EXIST_MSG, self::ERROR_USER_EXIST_CODE);
+        }
+
+        $userObj = new User();
+        $userObj->setEmail($emailAddress);
+        $userObj->setPassword($password);
+        $userObj->setRoles(array());
+        $userObj->setCreatedAt(new \DateTime());
+        $userObj->setIsEmailConfirmed(false);
+        $userObj->setIsActivated(false);
+        $this->entityManager->persist($userObj);
+
+        $this->entityManager->flush();
+
+        return $userObj;
     }
 
     public function forgotPassword($emailAddress)
--- module/MyCompany/tests/Service/UserServiceTest.php
+++ module/MyCompany/tests/Service/UserServiceTest.php
@@ -2,6 +2,7 @@
 namespace MyCompany\Service;
 
 use MyCompany\Bootstrap;
+use MyCompany\Entity\User;
 use MyCompany\Service\UserService;
 use PHPUnit_Framework_TestCase;
 
@@ -18,6 +19,14 @@ class UserServiceTest extends PHPUnit_Framework_TestCase
      */
     private $userService;
 
+
+    protected function getORM()
+    {
+        $sm = Bootstrap::getServiceManager();
+        $orm = $sm->get('doctrine.entitymanager.orm_default');
+        return $orm;
+    }
+
     protected function setUp()
     {
         parent::setUp();
@@ -25,6 +34,24 @@ class UserServiceTest extends PHPUnit_Framework_TestCase
         Bootstrap::init();
 
         $this->userService = Bootstrap::getServiceManager()->get(UserService::class);
+
+        $orm = $this->getORM();
+        $qb = $orm->createQueryBuilder();
+        $exp = $qb->expr();
+        $qb->select('u');
+        $qb->from(User::class,'u');
+        $qb->andWhere($exp->like('u.email', ':email'));
+        $qb->setParameter('email','%unit_test%');
+        $iterateResult = $qb->getQuery();
+        $res = $iterateResult->iterate();
+        foreach ($res as $usAsArr){
+            $orm->remove($usAsArr[0]);
+        }
+        $orm->flush();
+        $orm->clear();
+
+
+
     }
 
 
@@ -49,10 +76,25 @@ class UserServiceTest extends PHPUnit_Framework_TestCase
 
     public function testRegisterUser()
     {
-        $email = 'test@test.lt';
+        $email = 'ruslan+unit_test@test.lt';
         $password = 'abc1234';
         $userObj = $this->userService->registerUser($email,$password);
         $this->assertInstanceOf(User::class,$userObj);
+    }
+
+
+    public function testRegisterUserMailAlreadyExistsException()
+    {
+        $email = 'ruslan+unit_test@test.lt';
+        $password = 'abc1234';
+        $userObj = $this->userService->registerUser($email,$password);
+
+        $this->assertInstanceOf(User::class,$userObj);
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage(UserService::ERROR_USER_EXIST_MSG);
+        $userObj = $this->userService->registerUser($email,$password);
+
     }
 
     public function testForgotPassword()
